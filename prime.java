bbd6f0ed479a25bfdc8f4d650fe5264dca699016
vamsi8374 <vunnamvamsi540@gmail.com>
2023-05-04 14:42:10 -0500

    first commit

---
+++ prime.java
@@ -0,0 +1,9 @@
+public class prime{
+public static void main(String [] args){
+int n=23;
+if(n/2==0);
+system.out.println("its a composite number");
+else
+system.out.println("its a prime number");
+}
+}
